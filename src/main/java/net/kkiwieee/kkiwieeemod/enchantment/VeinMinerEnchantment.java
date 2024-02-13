efc6ce0c3e57db346c829a4722a57f12ae0629fc
krunchyXtac0 <elijahcichoski@gmail.com>
2024-02-12 18:17:54 -0800

    Added Start to VeinMinerEnchantment

---
+++ src/main/java/net/kkiwieee/kkiwieeemod/enchantment/VeinMinerEnchantment.java
@@ -0,0 +1,22 @@
+package net.kkiwieee.kkiwieeemod.enchantment;
+
+import net.minecraft.enchantment.Enchantment;
+import net.minecraft.enchantment.EnchantmentTarget;
+import net.minecraft.entity.EquipmentSlot;
+
+public class VeinMinerEnchantment extends Enchantment {
+    public VeinMinerEnchantment(Rarity rarity, EnchantmentTarget target, EquipmentSlot... slotTypes) {
+        super(rarity, target, slotTypes);
+    }
+
+    @Override
+    public int getMinPower(int level) {
+        return 15;
+    }
+
+    @Override
+    public int getMaxLevel() {
+        return 1;
+    }
+
+}
